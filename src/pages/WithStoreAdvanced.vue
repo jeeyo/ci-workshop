1a644c9b84f850c2a0d7a4c27dcacc7f6d56ba23
Natchanon Nuntanirund <jeeyo@outlook.com>
2023-02-12 02:45:09 +0700

    fix initialstate.txt path (#3)

--- src/pages/WithStoreAdvanced.vue
+++ src/pages/WithStoreAdvanced.vue
@@ -2,7 +2,7 @@
 import AdvancedCounterStoreFactory from '../stores/CounterStoreAdvanced'
 
 const fetchInitialState = async () => {
-  const response = await fetch('/initialState.txt')
+  const response = await fetch('initialState.txt')
   const data = await response.json() as { counter: number }
   return data
 }
